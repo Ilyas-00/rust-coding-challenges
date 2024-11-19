cf49cfb20b15047409db31144bd3b104b751a883
Ilyas-00 <akioui.ilyas@gmail.com>
2024-11-19 21:45:53 +0100

    Add number_pyramid.rs and even_numbers_array.rs files

---
+++ even_numbers_array.rs
@@ -0,0 +1,15 @@
+//! Problem Statement
+// Declare an array, arr, of size 6 that has numbers divisible by 2 ranging from 0 to 10.
+
+// Print the value of arr
+
+// Output
+// The output of the code should be:
+
+//    0,2,4,6,8,10
+// Coding Exercise#
+
+fn test() {
+    let array:[i32;6] = [0, 2, 4, 6, 8, 10]; 
+    print!("{},{},{},{},{},{}", array[0], array[1], array[2], array[3], array[4], array[5]); 
+   }
---
+++ number_pyramid.rs
@@ -0,0 +1,23 @@
+//! Problem Statement
+// The task is to display the pyramid of numbers in the following format.
+
+// Output
+// 1
+// 22
+// 333
+// 4444
+// 55555
+
+// Coding Exercise
+
+fn test() {
+    println!("{}", 1); 
+    println!("{}", 22); 
+    println!("{}", 333); 
+    println!("{}", 4444); 
+    println!("{}", 55555); 
+ }
+
+fn main() {
+    test();
+}
