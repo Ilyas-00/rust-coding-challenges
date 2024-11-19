ed7cc68177b773e5ec235ff8124bf3db18276a74
Ilyas-00 <akioui.ilyas@gmail.com>
2024-11-19 21:36:40 +0100

    Add print_statement.rs file

---
+++ print_statement.rs
@@ -0,0 +1,10 @@
+//! Coding Exercise#
+//! Write your code below. It is recommended​ that you try solving the exercise yourself before viewing the solution.
+
+fn test() {
+    println!("I am learning Rust programming language."); 
+}
+
+fn main() {
+    test();
+}
